7813ac0c932db65bbd83ec7951a58bf275c1cf80
Yifei Li <workingyifei@gmail.com>
2017-06-24 00:38:56 -0700

    initial commit, corrected openssl path in install-mac.sh

--- install-mac.sh
+++ install-mac.sh
@@ -5,9 +5,13 @@ cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+# export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export OPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2l
+
+
 cd build
-cmake ..
+# cmake -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2k -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/1.0.2k/lib
+cmake -DOPENSSLROOT_DIR=/usr/local/Cellar/openssl/1.0.2l  -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/1.0.2l/lib ..
 make 
 sudo make install
 cd ..
